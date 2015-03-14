7a8f4edd3b9118ce038b1f7cd070ff03b0d687a2
imraazrally <raisoft2020@gmail.com>
2015-03-14 10:35:20 -0700

    Update

---
+++ Simple Linked List (C)/linklst.c
@@ -0,0 +1,105 @@
+// C coding
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+struct clip {
+  int views;
+  char *user;
+  char *id;
+  char *dur;
+  char *title;
+  struct clip *next;
+};
+
+struct clip * head=NULL;
+
+//Function Prototypes
+void print();
+void getdata();
+void split_line(char * line);
+
+int main(int argc, char *argv[])
+{
+	getdata();
+	print();
+}
+
+void getdata(){
+    FILE * file=fopen("youtube.csv", mode);
+	char * mode="r";
+	char line[255]="";
+
+	if (file==NULL){
+		printf("File could not be open");	
+	}else{
+		while (fgets(line, sizeof(line), file)){
+			//printf("%s", line);
+			split_line(line);
+		}
+	}
+}
+
+void split_line(char * line){
+	int pos=0, views=0, len=0;
+	char * token=NULL;
+	char * tmp_string=NULL;
+	char * user=NULL;
+	char * dur=NULL	;
+	char * title=NULL;
+	char * id=NULL;
+	struct clip * tmp=NULL;
+	struct clip * current=NULL;
+	tmp=malloc(sizeof(struct clip));
+	
+	//views
+	token=strtok(line,",");
+	tmp->views=atoi(token);
+	//user
+	token=strtok(NULL,",");
+	tmp_string=malloc(sizeof(char)*strlen(token));
+	strcpy(tmp_string,token);
+	tmp->user=tmp_string;
+	//dur
+	token=strtok(NULL,",");
+	tmp_string=malloc(sizeof(char)*strlen(token));
+	strcpy(tmp_string,token);
+	tmp->dur=tmp_string;
+	//title
+	token=strtok(NULL,",");
+	tmp_string=malloc(sizeof(char)*strlen(token));
+	strcpy(tmp_string,token);
+	tmp->title=tmp_string;
+	//id
+	token=strtok(NULL,",");
+	tmp_string=malloc(sizeof(char)*strlen(token));
+	strcpy(tmp_string,token);
+	tmp->id=tmp_string;
+	tmp->next=NULL;
+
+	if (head==NULL){
+		head=tmp;
+	}else{
+		current=head;
+		while(current->next){
+			current=current->next;
+		}
+		current->next=tmp;
+	}
+}
+
+void print(){
+	struct clip * current=head;
+	while(current)
+	{	
+		printf("%d %s, %s, %s, %s \n",current->views,current->user,current->dur,current->title,current->id);	
+		current=current->next;
+	}
+}
+
+
+
+
+
+
+
+
---
+++ Simple Linked List (C)/readme_list.txt
@@ -0,0 +1,3 @@
+Given a CSV file containing, views, user, id, titles (Youtube.com),
+1. Use strtok() function to split a line into four fields
+2. Append each element into the linked list.
