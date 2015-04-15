19487f0438056cf88030b6d2d69a2953abd1f90c
imraazrally <raisoft2020@gmail.com>
2015-04-14 20:01:29 -0700

    update files

--- C Programming Practice/3 State-Space-Search/Move.h
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "Search.h"
-#ifndef MOVE_H
-#define MOVE_H
-
-typedef enum{UP, DOWN, LEFT, RIGHT} tile_pos;
-struct node * move(struct node *selected, tile_pos position);
-#endif
--- C Programming Practice/Simple Linked List (C)/readme_list.txt
+++
@@ -1,3 +0,0 @@
-Given a CSV file containing, views, user, id, titles (Youtube.com),
-1. Use strtok() function to split a line into four fields
-2. Append each element into the linked list.
